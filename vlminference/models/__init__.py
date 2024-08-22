95b1cdb5e457c1ca8a7160fafa652112842f4b08
gxlover0625 <2031808280@qq.com>
2024-08-22 16:30:59 +0800

    update minicpm

--- vlminference/models/__init__.py
+++ vlminference/models/__init__.py
@@ -1,8 +1,19 @@
-from .internvl2 import InternVL2ForInfer, InternVL2ForInferBasic
-from .minicpmv import MiniCPMVForInfer
+import os
 
-__all__ = [
-    "InternVL2ForInfer",
-    "InternVL2ForInferBasic",
-    "MiniCPMVForInfer",
-]
+# 获取环境变量
+model_env = os.getenv('MODEL_ENV')
+
+# 根据环境变量导入相应的模型
+if model_env == 'internvl2':
+    from .internvl2 import InternVL2ForInfer, InternVL2ForInferBasic
+    __all__ = [
+        "InternVL2ForInfer",
+        "InternVL2ForInferBasic",
+    ]
+elif model_env == 'minicpmv':
+    from .minicpmv import MiniCPMVForInfer
+    __all__ = [
+        "MiniCPMVForInfer",
+    ]
+else:
+    raise ValueError(f"Unknown MODEL_ENV: {model_env}")
