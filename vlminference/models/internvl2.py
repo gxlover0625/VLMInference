089fbfd73b893da3e9b416e8bf78492fc0bd6210
gxlover0625 <2031808280@qq.com>
2024-09-01 16:00:27 +0800

    update internvl

--- vlminference/models/internvl2.py
+++ vlminference/models/internvl2.py
@@ -14,14 +14,14 @@ from transformers import AutoModel, AutoTokenizer
 from ..inference import InferenceEngine
 
 class InternVL2ForInfer(InferenceEngine):
-    def __init__(self, model_path = None, system_prompt = None, context_max_len = 8192, max_new_tokens = 512, top_p = 1.0, top_k = 1, temperature = 0.8, repetition_penalty = 1.0):
+    def __init__(self, model_path = None, system_prompt = None, context_max_len = 8192, cache_max_entry_count = 0.8, max_new_tokens = 512, top_p = 1.0, top_k = 1, temperature = 0.8, repetition_penalty = 1.0):
         assert model_path is not None, "Please provide model path"
         if system_prompt is None:
             system_prompt = '我是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。'
         chat_template_config = ChatTemplateConfig('internvl-internlm2')
         chat_template_config.meta_instruction = system_prompt
         self.gen_config = GenerationConfig(max_new_tokens=max_new_tokens, top_p=top_p, top_k=top_k, temperature=temperature, repetition_penalty=repetition_penalty)
-        self.model = pipeline(model_path, chat_template_config=chat_template_config, backend_config=TurbomindEngineConfig(session_len=context_max_len))
+        self.model = pipeline(model_path, chat_template_config=chat_template_config, backend_config=TurbomindEngineConfig(tp=torch.cuda.device_count(), session_len=context_max_len, cache_max_entry_count=cache_max_entry_count))
     
     def load_image(self, img_url):
         return load_image(img_url)
@@ -249,6 +249,7 @@ class InternVL2ForInferBasic(InferenceEngine):
             num_patches_list = [img.size(0) for img in images]
             images = torch.cat(images, dim=0)
             response_list = self.model.batch_chat(self.tokenizer, images, prompts, num_patches_list=num_patches_list, generation_config=self.gen_config)
+            
             del inputs_list, prompts, num_patches_list, images
             torch.cuda.empty_cache()
             return response_list
