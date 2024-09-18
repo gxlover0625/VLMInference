882b44a302d9d0fab1c3ddff76b3494c5b2a1769
gxlover0625 <2031808280@qq.com>
2024-09-18 22:11:44 +0800

    update minicpm

--- vlminference/scripts/batch_infer.sh
+++ vlminference/scripts/batch_infer.sh
@@ -1,5 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python3 ../utils/batch_infer.py \
 --model_name "InternVL2-8B" \
 --model_path "/root/autodl-tmp/llm/OpenGVLab/InternVL2-8B" \
+--batch_size 8 \
 --input_file "/root/autodl-tmp/pub/VLMInference/test.jsonl" \
 --output_file "/root/autodl-tmp/codes/test/otuput.jsonl"
